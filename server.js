d3bf3340b6e497df7feb071dfc1a73bd718353e7
Nick Moore <nickmoore04@gmail.com>
2018-03-26 17:39:57 -0600

    updating things

--- server.js
+++ server.js
@@ -11,16 +11,6 @@ let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/HeadlinesDB';
 mongoose.connect(MONGODB_URI);
 mongoose.Promise = Promise;
 
-let db = mongoose.connection;
-// Show any mongoose errors
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
-});
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
-
 // express
 let app = express();
 
